15ad25c7aaa79801cc1e0780af444a1edffd76cd
Chrithyan007 <138399577+Chrithyan007@users.noreply.github.com>
2023-11-02 14:45:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function whats() {
+    var whatsappLink = "https://wa.me/5594991991920";
+    window.open(whatsappLink, '_blank');
+}
+
+function insta() {
+var instagramLink = 'https://instagram.com/cl_tech01?igshid=MzMyNGUyNmU2YQ==';
+window.open(instagramLink, '_blank');
+}
+function cel() {
+
+var phoneLink = 'tel:94991991920';
+window.location.href = phoneLink;
+}
